04407e03c4d5f6969d08ec7f44ac5b52b93de6e8
Bernardo-G-Cunha <bernardo.garcia.cunha@gmail.com>
2026-05-30 16:51:44 -0300

    feat: add household profile exploratory analysis

---
+++ sql/churn_by_partner.sql
@@ -0,0 +1,9 @@
+SELECT
+    Partner,
+    COUNT(*) AS customers,
+    ROUND(
+        AVG(CASE WHEN Churn = 'Yes' THEN 1 ELSE 0 END) * 100,
+        2
+    ) AS churn_rate
+FROM customers
+GROUP BY Partner;
